207437c3d60ebc910cf1e3e92879d437205297f6
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-09-07 13:45:40 +0200

    set up a default configuration (baseURL and headers)

--- ex4/src/containers/Blog/Blog.js
+++ ex4/src/containers/Blog/Blog.js
@@ -15,7 +15,7 @@ class Blog extends Component {
 
     componentDidMount() {
         axios
-            .get('https://jsonplaceholder.typicode.com/postsss')
+            .get('/posts')
             .then(response => {
                 const posts = response
                     .data
