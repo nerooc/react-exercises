afa20b57e3a09bf476889769cd301c56ac9a9bdd
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-09-10 20:15:48 +0200

    added lazy loading for NewPost component

--- ex5 - routing/src/containers/Blog/Blog.js
+++ ex5 - routing/src/containers/Blog/Blog.js
@@ -2,11 +2,20 @@ import React, {Component} from 'react';
 // import axios from 'axios';
 import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+//import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
 
 import './Blog.css';
 
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+
+});
+
 class Blog extends Component {
+    state = {
+        auth: true
+    }
 
     render() {
         return (
@@ -37,9 +46,11 @@ class Blog extends Component {
                 </header>
                 {/* <Route path="/" exact render={() => <Posts/>}/> */}
                 <Switch>
-                    <Route path="/new-post" exact component={NewPost}/>
+                    {this.state.auth
+                        ? <Route path="/new-post" exact component={AsyncNewPost}/>
+                        : null}
                     <Route path="/posts" component={Posts}/>
-                    <Redirect from="/" to="/posts"/>
+                    <Route render={() => <h1>Not found</h1>}/> {/* <Redirect from="/" to="/posts"/> */}
                 </Switch>
             </div>
 
